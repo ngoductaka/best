d1ce417cc2eb817bd8a1ace3254d82669eb3a219
ngoductaka <ngocductaka@gmail.com>
2019-08-22 11:13:43 +0700

    Create sortting.js

---
+++ sortting.js
@@ -0,0 +1,28 @@
+function selectionSort(array) {
+  // change code below this line
+  for(let i = 0; i<array.length-1; i++){
+    let min = array[i];
+    let tem = i;
+    for(let j = i+1; j < array.length; j++){
+      if(array[j]<min) { min = array[j], tem = j}
+    }
+    [array[i],array[tem]] = [array[tem],array[i]]
+  }
+  // change code above this line
+  return array;
+}
+var inp = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+selectionSort(inp);
+
+function bubbleSort(array) {
+  // change code below this line
+  for(let i = 0; i<array.length-1; i++){
+    for(let j = i+1; j < array.length; j++){
+      if(array[i] > array[i]) {
+        [array[i],array[j]] = [array[j],array[i]]
+      }
+    }
+  }
+  // change code above this line
+  return array;
+}

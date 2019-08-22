9dcdbac5c5917397755745ef738562127eb06dc5
ngoductaka <ngocductaka@gmail.com>
2019-08-22 11:41:45 +0700

    Update sortting.js

--- sortting.js
+++ sortting.js
@@ -26,3 +26,20 @@ function bubbleSort(array) {
   // change code above this line
   return array;
 }
+/////////////
+function insertionSort(array) {
+  // change code below this line
+  for(let i = 1; i < array.length; i++) {
+    for(let j = 0; j<=i; j++) {
+      if( array[i]<array[j] ) {
+        array.splice(j,0, array[i]);
+        array.splice(i+1,1);
+      }
+    }
+  }
+  // change code above this line
+  return array;
+}
+
+// test array:
+// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
